342f9db504cc271d97ba9f8701a8f851bb59f189
FlyFrog <ramgear@gmail.com>
2013-08-15 00:18:07 +0700

    MSM: Add GPU OC 487/533 MHz support

--- arch/arm/mach-msm/lge/mako/board-mako-gpu.c
+++ arch/arm/mach-msm/lge/mako/board-mako-gpu.c
@@ -223,17 +223,21 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 #ifdef CONFIG_GPU_OVERCLOCK
 		{
+			.gpu_freq = 533333000,
+			.bus_freq = 4,
+			.io_fraction = 0,
+		},
+		{
 			.gpu_freq = 487500000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
-#else
+#endif
         {
 			.gpu_freq = 400000000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
-#endif
 		{
 			.gpu_freq = 320000000,
 			.bus_freq = 3,
@@ -255,7 +259,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 1,
-	.num_levels = 5,
+	.num_levels = KGSL_MAX_PWRLEVELS,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
