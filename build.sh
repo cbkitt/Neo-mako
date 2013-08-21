da3b12a70456bd704133d14e1312c3bdf4c55671
FlyFrog <ramgear@gmail.com>
2013-08-21 19:27:03 +0700

    Update build script

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ VER="-000"
 BUILD_VER=$BASE_VER$VER
 
 export LOCALVERSION="~"`echo $BUILD_VER`
-#export CROSS_COMPILE=/opt/toolchain/arm-eabihf-4.7-2013.04/bin/arm-linux-gnueabihf-
 export CROSS_COMPILE=/opt/toolchain/arm-eabihf-4.8-2013.08/bin/arm-linux-gnueabihf-
 export ARCH=arm
 export SUBARCH=arm
@@ -19,8 +18,9 @@ DATE_START=$(date +"%s")
 KERNEL_DIR=`pwd`
 OUTPUT_DIR=${HOME}/Custom-Kernel
 ZIMAGE_DIR=$KERNEL_DIR/arch/arm/boot
-CWM_DIR=../NEO_CWM
-CWM_KERNEL_DIR=$CWM_DIR/kernel
+RAMDISK_DIR=$KERNEL_DIR/../Neo-Ramdisk
+INITRD_DIR=$RAMDISK_DIR/initrd
+CWM_DIR=$RAMDISK_DIR/cwm
 MODULES_DIR=$CWM_DIR/system/lib/modules
 
 echo 
@@ -29,35 +29,46 @@ echo
 echo "LOCALVERSION="$LOCALVERSION
 echo "CROSS_COMPILE="$CROSS_COMPILE
 echo "ARCH="$ARCH
+echo "SUBARCH="$SUBARCH
+echo "KBUILD_BUILD_USER="$KBUILD_BUILD_USER
+echo "KBUILD_BUILD_HOST="$KBUILD_BUILD_HOST
+echo
 echo "KERNEL_DIR="$KERNEL_DIR
+echo "RAMDISK_DIR="$RAMDISK_DIR
 echo "OUTPUT_DIR="$OUTPUT_DIR
-echo "ZIMAGE_DIR="$ZIMAGE_DIR
-echo "MODULES_DIR="$MODULES_DIR
 echo
 
 echo 
 echo "Making defconfig"
 echo
 
+# make
 make "mako_defconfig"
 make -j3 > ~/make.log
 
+# copy output files
 rm `echo $MODULES_DIR"/*"`
 find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+cp -vr $ZIMAGE_DIR/zImage $RAMDISK_DIR
 
-# cd $CWM_KERNEL_DIR
-cp -vr $ZIMAGE_DIR/zImage $CWM_KERNEL_DIR
-# use randisk modification script in CWM folder
-#./mkbootimg --kernel zImage --ramdisk initrd.img --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677' --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81800000 -o boot.img
+# create ramdisk
+cd $INITRD_DIR
+find . | cpio -o -H newc | gzip > ../ramdisk.cpio.gz
 
-#cp -vr boot.img $CWM_DIR
+# create boot.img
+cd $RAMDISK_DIR
+./mkbootimg --kernel zImage --ramdisk ramdisk.cpio.gz --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677' --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81800000 -o boot.img
+
+# create flashable zip
+mv boot.img $CWM_DIR
 cd $CWM_DIR
 zip -r `echo $BUILD_VER`.zip *
 mv  `echo $BUILD_VER`.zip $OUTPUT_DIR
 
-# rm -rf $RAM_DIR/zImage
-# rm -rf $RAM_DIR/initrd.img
-# rm -rf $RAM_DIR/boot.img
+# clean 
+rm -rf $RAMDISK_DIR/zImage
+rm -rf $RAMDISK_DIR/ramdisk.cpio.gz
+rm -rf $CWM_DIR/boot.img
 
 echo
 echo "Build completed at "$OUTPUT_DIR"/"$BUILD_VER".zip"
