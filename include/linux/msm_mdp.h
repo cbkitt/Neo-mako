1f11d9cc9647cb38481ec77a5d132b6161e6835a
FlyFrog <ramgear@gmail.com>
2013-09-13 18:57:54 +0700

    Use common JSS/JWR code Conflicts: 	Makefile

--- include/linux/msm_mdp.h
+++ include/linux/msm_mdp.h
@@ -573,7 +573,17 @@ struct mdp_buf_sync {
 	uint32_t acq_fen_fd_cnt;
 	int *acq_fen_fd;
 	int *rel_fen_fd;
+#ifdef CONFIG_FB_MSM_JWR
+};
+
+struct mdp_buf_fence {
+	uint32_t flags;
+	uint32_t acq_fen_fd_cnt;
+	int acq_fen_fd[MDP_MAX_FENCE_FD];
+	int rel_fen_fd[MDP_MAX_FENCE_FD];
+#else
 	int *retire_fen_fd;
+#endif
 };
 
 #define MDP_DISPLAY_COMMIT_OVERLAY 0x00000001
